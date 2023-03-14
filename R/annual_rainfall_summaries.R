f7432bbd84380793a88e4c57340b72554a1507a8
Danny Parsons <danny@idems.international>
2023-03-14 18:01:42 +0000

    added link to epicsadata package

--- R/annual_rainfall_summaries.R
+++ R/annual_rainfall_summaries.R
@@ -1,5 +1,6 @@
 #' Annual Rainfall Summaries
 #'
+#' @param country `character(1)` The country code of the data.
 #' @param station_id `character` The id's of the stations to analyse. Either a
 #'   single value or a vector.
 #' @param summaries `character` The names of the summaries to produce.
@@ -8,15 +9,18 @@
 #' @export
 #'
 #' @examples
-#' annual_rainfall_summaries(station_id = "1357")
-annual_rainfall_summaries <- function(station_id, 
+#' # annual_rainfall_summaries(country = "zm", station_id = "01122")
+annual_rainfall_summaries <- function(country,
+                                      station_id, 
                                       summaries = c("seasonal_rainfall",
                                                     "seasonal_raindays",
                                                     "start_rains",
                                                     "end_season",
                                                     "length_season")) {
-  df <- data.frame(station_id = rep(station_id, each = 30),
-                   year = rep(1991:2020, 2))
-  df$seasonal_rainfall <- df$year/3
-  return(df)
+  daily <- epicsadata::get_daily_data(country = country, 
+                                      station_id = station_id)
+  annual <- rpicsa::annual_rain(daily, date_time = "date", rain = "rain",
+                                year = "year", station = "station",
+                                na_rm = FALSE)
+  return(annual)
 }
