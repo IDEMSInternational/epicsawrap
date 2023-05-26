2df6346518b053e2f30b201868e67739ddaeb71d
lilyclements <lily@idems.international>
2023-05-26 15:22:37 +0100

    definitions update

--- R/definitions.R
+++ R/definitions.R
@@ -1,3 +1,13 @@
+#' Definitions
+#'
+#' @param country `character(1)` The country code of the data.
+#' @param station_id `character(1)` The station code in the data.
+#' @param summaries `character` Vector of summaries to display
+#'
+#' @return TODO
+#' @export
+#'
+#' @examples #TODO
 definitions <- function(country, station_id, summaries){
   definition_data <- epicsadata::get_definitions_data(country = country, station_id = station_id)
   definition_data <- purrr::map(.x = summaries, .f = ~ definition_data[[.x]])
