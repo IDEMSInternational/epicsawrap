034fbd3add596de2207e243e39f09b88c6bc464c
lilyclements <lily@idems.international>
2023-06-21 21:49:34 +0300

    Add parameters for crop_success_probabilities

--- R/crop_success_probabilities.R
+++ R/crop_success_probabilities.R
@@ -14,22 +14,49 @@
 #' #epicsawrap::setup(dir = getwd())
 #' #epicsawrap::gcs_auth_file(file = "C:/Users/lclem/Downloads/e-picsa-e630400792e7.json")
 #' #crop_success_probabilities(country = "zm", station_id = "16")
+#' 
+#' # or some can be defined in the dialog
+#' #crop_success_probabilities(country = "zm", station_id = "16", water_requirements = c(100, 300, 800))
 crop_success_probabilities <- function(country,
-                                       station_id) {
+                                       station_id,
+                                       planting_dates = NULL,
+                                       water_requirements = NULL,
+                                       crop_length = NULL,
+                                       start_before_season = NULL) {
   daily <- epicsadata::get_daily_data(country = country, station_id = station_id)
   if ("station_name" %in% names(daily)) daily$station <- daily$station_name # temp until we don't hard code in the columns call
   definitions <- epicsawrap::definitions(country = country, station_id = station_id, summaries = "crops_success")
   # TODO: call sor/eor if it is already run?
   season_data <- annual_rainfall_summaries(country = country, station_id = station_id, summaries = c("start_rains", "end_rains")) # end rains or end season?
-  summary_crops <- rpicsa::crops_definitions(data = daily,
+  if (is.null(crop_length)){
+    crop_length <- as.numeric(definitions$crops_success$plant_lengths)
+  } else {
+    definitions$crops_success$plant_lengths <- crop_length
+  }
+  if (is.null(water_requirements)){
+    water_requirements <- as.numeric(definitions$crops_success$rain_totals)
+  } else {
+    definitions$crops_success$rain_totals <- water_requirements
+  }
+  if (is.null(planting_dates)) {
+    planting_dates <- as.numeric(definitions$crops_success$plant_days)
+  } else {
+    definitions$crops_success$plant_days <- planting_dates
+  }
+  if (is.null(start_before_season)){
+    start_before_season <- is.logical(definitions$crops_success$start_check)
+  } else {
+    definitions$crops_success$start_check <- start_before_season
+  }
+  summary_crops <- crops_definitions(data = daily,
                                              date_time  = "date",
                                              station = "station",
                                              year = "year",
                                              rain = "rain",
-                                             rain_totals = as.numeric(definitions$crops_success$rain_totals),
-                                             plant_days = as.numeric(definitions$crops_success$plant_days),
-                                             plant_lengths = as.numeric(definitions$crops_success$plant_lengths),
-                                             start_check = is.logical(definitions$crops_success$start_check),
+                                             rain_totals = water_requirements,
+                                             plant_days = planting_dates,
+                                             plant_lengths = crop_length,
+                                             start_check = start_before_season,
                                              season_data = season_data[[2]],
                                              start_day = "start_rain",
                                              end_day = "end_rain")
