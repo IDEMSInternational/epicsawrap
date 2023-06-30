b6c7a9a8bcae8bb9be8542a9d5da2149f55e71d9
lilyclements <lily@idems.international>
2023-06-30 17:02:43 +0100

    Update crop_success_probabilities.R
    Fixes #33

--- R/crop_success_probabilities.R
+++ R/crop_success_probabilities.R
@@ -55,7 +55,7 @@ crop_success_probabilities <- function(country,
   } else {
     definitions$crops_success$start_check <- start_before_season
   }
-  summary_crops <- cd(data = daily,
+  summary_crops <- rpicsa::crops_definitions(data = daily,
                                              date_time  = data_names$date,
                                              station = data_names$station,
                                              year = data_names$year,
