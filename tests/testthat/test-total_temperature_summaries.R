8389c514537863675479c5ef60725ea500d20696
lilyclements <lily@idems.international>
2023-10-25 16:47:59 +0100

    Update test-total_temperature_summaries.R

--- tests/testthat/test-total_temperature_summaries.R
+++ tests/testthat/test-total_temperature_summaries.R
@@ -7,10 +7,11 @@ test_1_annual <- readRDS("tests/test_1_annual.rds")
 test_1_monthly <- readRDS("tests/test_1_monthly.rds")
 country <- "zm"
 station_id <- "test_1"
-result_annual <- annual_temperature_summaries(country, station_id)
-result_monthly <- monthly_temperature_summaries(country, station_id)
 
 test_that("Correct summaries are calculated", {
+  result_annual <- annual_temperature_summaries(country, station_id)
+  result_monthly <- monthly_temperature_summaries(country, station_id)
+  
   expect_true(identical(result_annual[[2]], test_1_annual))
   expect_true(identical(result_monthly[[2]], test_1_monthly))
 })
