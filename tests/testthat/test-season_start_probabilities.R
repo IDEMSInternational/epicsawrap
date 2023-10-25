d27d32ab91e153cdfecff1816d2d85fcff71933a
lilyclements <lily@idems.international>
2023-10-25 16:47:11 +0100

    Update test-season_start_probabilities.R

--- tests/testthat/test-season_start_probabilities.R
+++ tests/testthat/test-season_start_probabilities.R
@@ -1,12 +1,12 @@
 library(testthat)
-library(rpicsa)
+library(epicsawrap)
 
 # Test case 1
-epicsawrap::gcs_auth_file(file = "tests/e-picsa-e630400792e7.json")
+gcs_auth_file(file = "tests/e-picsa-e630400792e7.json")
 test_1_results <- readRDS("tests/test_1_season_start_probabilities.rds")
 country <- "zm"
 station_id <- "test_1"
-result <- epicsawrap::season_start_probabilities(country, station_id, c(0, 150, 300))
+result <- season_start_probabilities(country, station_id, c(0, 150, 300))
 test_that("Correct summaries are calculated", {
   expect_true(identical(result[[2]], test_1_results))
 })
