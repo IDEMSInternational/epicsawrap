a34c87f6eeeb7281526b74dcfe71cefc507783de
lilyclements <lily@idems.international>
2023-10-25 16:47:27 +0100

    Update test-total_temperature_summaries.R

--- tests/testthat/test-total_temperature_summaries.R
+++ tests/testthat/test-total_temperature_summaries.R
@@ -1,14 +1,14 @@
 library(testthat)
-library(rpicsa)
+library(epicsawrap)
 
 # Test case 1
-epicsawrap::gcs_auth_file(file = "tests/e-picsa-e630400792e7.json")
+gcs_auth_file(file = "tests/e-picsa-e630400792e7.json")
 test_1_annual <- readRDS("tests/test_1_annual.rds")
 test_1_monthly <- readRDS("tests/test_1_monthly.rds")
 country <- "zm"
 station_id <- "test_1"
-result_annual <- epicsawrap::annual_temperature_summaries(country, station_id)
-result_monthly <- epicsawrap::monthly_temperature_summaries(country, station_id)
+result_annual <- annual_temperature_summaries(country, station_id)
+result_monthly <- monthly_temperature_summaries(country, station_id)
 
 test_that("Correct summaries are calculated", {
   expect_true(identical(result_annual[[2]], test_1_annual))
