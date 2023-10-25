5ea901d30310d0be109ee09255bf490a318f97c8
lilyclements <lily@idems.international>
2023-10-25 16:48:13 +0100

    Update test-season_start_probabilities.R

--- tests/testthat/test-season_start_probabilities.R
+++ tests/testthat/test-season_start_probabilities.R
@@ -6,7 +6,7 @@ gcs_auth_file(file = "tests/e-picsa-e630400792e7.json")
 test_1_results <- readRDS("tests/test_1_season_start_probabilities.rds")
 country <- "zm"
 station_id <- "test_1"
-result <- season_start_probabilities(country, station_id, c(0, 150, 300))
 test_that("Correct summaries are calculated", {
+  result <- season_start_probabilities(country, station_id, c(0, 150, 300))
   expect_true(identical(result[[2]], test_1_results))
 })
