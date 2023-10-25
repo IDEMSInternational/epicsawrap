ca25d96dce6efd6164c5690029aeac47304c4dc6
lilyclements <lily@idems.international>
2023-10-25 14:53:38 +0100

    crop success probabilities test

--- tests/testthat/test-crop_success_probabilities.R
+++ tests/testthat/test-crop_success_probabilities.R
@@ -1,40 +1,13 @@
-# library(testthat)
-# 
-# # External functions call
-# # Test case 1: Test with all parameter
-# result <- epicsawrap::crop_success_probabilities(
-#   country = "zm",
-#   station_id = "16",
-#   planting_dates = c(92, 122, 153),
-#   water_requirements = c(300, 500, 700),
-#   planting_length = c(120, 180),
-#   start_before_season = TRUE
-# )
-# 
-# # Test case 2: Test with missing planting_length parameter
-# result_2 <- epicsawrap::crop_success_probabilities(
-#   country = "zm",
-#   station_id = "16",
-#   planting_dates = c(1, 2, 3),
-#   water_requirements = c(100, 200, 300),
-#   start_before_season = TRUE
-# )
-# 
-# # Test case 2: Test with all missing parameters
-# result_3 <- epicsawrap::crop_success_probabilities(
-#   country = "zm",
-#   station_id = "16"
-# )
-# 
-# # Define test cases
-# test_that("crop_success_probabilities returns correct results", {
-#   expect_no_error(result)
-#   expect_no_error(result_2)
-#   expect_no_error(result_3)
-# })
-# 
-# # Define test cases
-# test_that("crop_success_probabilities returns same results whether parameters are defined or not", {
-#   expect_identical(result[[2]], result_3[[2]])
-# })
-# 
+library(testthat)
+library(rpicsa)
+
+# Test case 1
+epicsawrap::gcs_auth_file(file = "tests/e-picsa-e630400792e7.json")
+test_1_results <- readRDS("tests/test_1_crop_success_probabilities.rds")
+country <- "zm"
+station_id <- "test_1"
+result <- epicsawrap::crop_success_probabilities(country, station_id,
+                                                 water_requirements = c(0, 100, 300))
+test_that("Correct summaries are calculated", {
+  expect_true(identical(result[[2]], test_1_results))
+})
