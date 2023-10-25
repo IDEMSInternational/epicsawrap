3fbaea5e18da48e739b3f6664bd2d9d1e29afe1d
lilyclements <lily@idems.international>
2023-10-25 16:48:36 +0100

    Update test-crop_success_probabilities.R

--- tests/testthat/test-crop_success_probabilities.R
+++ tests/testthat/test-crop_success_probabilities.R
@@ -6,8 +6,7 @@ gcs_auth_file(file = "tests/e-picsa-e630400792e7.json")
 test_1_results <- readRDS("tests/test_1_crop_success_probabilities.rds")
 country <- "zm"
 station_id <- "test_1"
-result <- crop_success_probabilities(country, station_id,
-                                     water_requirements = c(0, 100, 300))
 test_that("Correct summaries are calculated", {
+  result <- crop_success_probabilities(country, station_id, water_requirements = c(0, 100, 300))
   expect_true(identical(result[[2]], test_1_results))
 })
