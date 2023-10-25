71a0decdf2f44c950e086194e7939b67b85868b4
lilyclements <lily@idems.international>
2023-10-25 15:21:50 +0100

    season_start_probabilities test

--- tests/testthat/test-season_start_probabilities.R
+++ tests/testthat/test-season_start_probabilities.R
@@ -1,20 +1,12 @@
-# library(testthat)
-# 
-# # External functions call
-# # Test case 1: Test with all parameters
-# result <-
-#   season_start_probabilities(
-#     country = "zm",
-#     station_id = "16",
-#     start_dates = c(10, 20, 100)
-#   )
-# 
-# # Test case 2: Test with missing parameters
-# result_2 <-
-#   season_start_probabilities(country = "zm", station_id = "16")
-# 
-# # Define test cases
-# test_that("season_start_probabilities returns correct results", {
-#   expect_no_error(result)
-#   expect_no_error(result_2)
-# })
+library(testthat)
+library(rpicsa)
+
+# Test case 1
+epicsawrap::gcs_auth_file(file = "tests/e-picsa-e630400792e7.json")
+test_1_results <- readRDS("tests/test_1_season_start_probabilities.rds")
+country <- "zm"
+station_id <- "test_1"
+result <- epicsawrap::season_start_probabilities(country, station_id, c(0, 150, 300))
+test_that("Correct summaries are calculated", {
+  expect_true(identical(result[[2]], test_1_results))
+})
