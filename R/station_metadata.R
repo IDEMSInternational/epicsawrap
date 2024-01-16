8e6cf9d5ed3e596de9b3ca25857454706ec9daca
lilyclements <lily@idems.international>
2024-01-16 13:02:53 +0000

    fixes issue #50

--- R/station_metadata.R
+++ R/station_metadata.R
@@ -1,11 +1,15 @@
 #' Get Station Metadata
 #'
-#' @param country `character(1)` The country code of the data.
-#'
-#' @return TODO
+#' This function retrieves metadata for weather stations in specified countries.
+#' @param country A character vector specifying the country or countries from which to get the metadata. Options include "zm" (Zambia) and "mz" (Mozambique).
+#' @param station_id A character vector specifying the station IDs to filter by. If provided, only metadata for the specified station IDs will be returned.
+#' @param include_definitions A logical value indicating whether to include definitions data. If TRUE, additional information about station definitions will be included in the output.
+#' @param format A character vector specifying the format of the output. Options are "wide" (default), "long", or "nested".
+#' @return If `include_definitions` is FALSE, the function returns a data frame with metadata for the specified stations. If `include_definitions` is TRUE, it returns a data frame with both metadata and station definitions.
+#' 
 #' @export
-#'
-#' @examples # TODO
-station_metadata <- function(country) {
-  epicsadata::station_metadata(country)
+station_metadata <- function(country = NULL, station_id = NULL, include_definitions = FALSE, format = c("wide", "long", "nested")) {
+  epicsadata::station_metadata(country = country, station_id = station_id,
+                               include_definitions = include_definitions,
+                               format = format)
 }
