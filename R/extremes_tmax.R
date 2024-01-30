c15978ee0b1b678cea204238c19f9fed88bde889
lilyclements <lily@idems.international>
2024-01-30 17:25:51 +0000

    export extremes functions

--- R/extremes_tmax.R
+++ R/extremes_tmax.R
@@ -6,10 +6,11 @@
 #' @param station_id A character vector specifying the ID(s) of the station(s) to analyse.
 #'
 #' @return A data frame containing the extreme data.
+#' @export
 #'
 #' @examples
 #' # Generate annual temperature summaries for station 16 in Zambia
 #' #extremes_rain(country, station_id)
 extremes_tmax <- function(country, station_id){
-  return(overall_extremes_summaries(country = country, station_id = station_id, summaries = "extremes_tmax"))
+  return(epicsawrap:::overall_extremes_summaries(country = country, station_id = station_id, summaries = "extremes_tmax"))
 }
