c1139f068a2ff2267c3415845bc48bf1edc56c7f
lilyclements <lily@idems.international>
2024-02-01 18:30:44 +0000

    testthat fixes

--- tests/testthat/test-annual_rainfall_summaries.R
+++ tests/testthat/test-annual_rainfall_summaries.R
@@ -9,7 +9,7 @@ country <- "zm"
 station_id <- "test_1"
 
 test_that("Correct summaries are calculated", {
-  result <- annual_rainfall_summaries(country, station_id)
+  result <- annual_rainfall_summaries1(country, station_id)
   expect_true(identical(result[[2]], test_1_results))
 })
 
