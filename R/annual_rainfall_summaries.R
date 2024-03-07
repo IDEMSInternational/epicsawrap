7dac77d99a002b3aaaa706b5d00e4eb6edf12ea5
lilyclements <lily@idems.international>
2024-03-07 17:50:15 +0000

    fix in factors

--- R/annual_rainfall_summaries.R
+++ R/annual_rainfall_summaries.R
@@ -81,7 +81,6 @@ annual_rainfall_summaries <- function(country, station_id, summaries = c("annual
   # For the variable names to be set as a certain default, set TRUE here, and run check_and_rename_variables
   data_names <- epicsadata::data_definitions(names(daily), TRUE)
   daily <- check_and_rename_variables(daily, data_names)
-  daily$year <- factor(daily$year)
   
   # Check if start_rains and end_rains are required for seasonal_rain and seasonal_length
   if (any(grepl("seasonal_", summaries))){
@@ -136,7 +135,7 @@ annual_rainfall_summaries <- function(country, station_id, summaries = c("annual
   }
 
   if (!is.null(definitions$start_rains$s_start_doy) | !is.null(definitions$end_season$s_start_doy) | !is.null(definitions$end_rains$s_start_doy)){
-    summary_data$year <- sub("-.*", "", summary_data$year)
+    summary_data$year <- factor(sub("-.*", "", summary_data$year))
   }
 
   if ("annual_rain" %in% summaries) {
