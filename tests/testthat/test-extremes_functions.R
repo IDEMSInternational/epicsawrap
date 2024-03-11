42961cd6ecf15e068774fcc2d71a597e812c9f1d
lilyclements <lily@idems.international>
2024-03-11 13:20:15 +0000

    extremes_summaries to call summary data frombucket

--- tests/testthat/test-extremes_functions.R
+++ tests/testthat/test-extremes_functions.R
@@ -4,11 +4,16 @@ library(testthat)
 
 # Test case 
 epicsadata::gcs_auth_file(file = "testdata/epicsa_token.json")
-test_1_results <- readRDS("testdata/extremes.rds")
+test_1_results <- readRDS("testdata/extremes_summaries_test_1.rds")
 country <- "zm"
 station_id <- "test_1"
 
+test_that("Correct summaries are called", {
+  result <- extremes_summaries1(country, station_id)[[2]]
+  expect_equal(nrow(result), nrow(test_1_results))
+})
+
 test_that("Correct summaries are calculated", {
-  result <- extremes_summaries(country, station_id)[[2]]
+  result <- extremes_summaries1(country, station_id, override = TRUE)[[2]]
   expect_equal(nrow(result), nrow(test_1_results))
 })
