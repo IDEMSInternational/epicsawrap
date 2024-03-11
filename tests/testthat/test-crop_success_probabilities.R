4568fcd192900c33ad338eb82df4ff6981dd49f6
lilyclements <lily@idems.international>
2024-03-11 13:09:11 +0000

    crop_success_probabilities - adding option to call from summary data

--- tests/testthat/test-crop_success_probabilities.R
+++ tests/testthat/test-crop_success_probabilities.R
@@ -4,11 +4,16 @@ library(epicsawrap)
 
 # Test case 1
 epicsadata::gcs_auth_file(file = "testdata/epicsa_token.json")
-test_1_results <- readRDS("testdata/test_1_crop_success_probabilities.rds")
+test_1_results <- readRDS("testdata/crop_success_probabilities_test_1.rds")
 country <- "zm"
 station_id <- "test_1"
 
-test_that("Correct summaries are calculated", {
+test_that("Correct summaries are called", {
   result <- crop_success_probabilities(country, station_id, water_requirements = c(0, 100, 300))
+  expect_true(identical(result[[2]], test_1_results))
+})
+
+test_that("Correct summaries are calculated", {
+  result <- crop_success_probabilities(country, station_id, water_requirements = c(0, 100, 300), override = TRUE)
   expect_true(identical(result[[2]], test_1_results))
 })
