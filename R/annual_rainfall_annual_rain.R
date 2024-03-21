721a5728a178f7681e94936200b3d1b6eefdd02d
lilyclements <lily@idems.international>
2024-03-21 16:43:47 +0200

    update annual_rain efinitions

--- R/annual_rainfall_annual_rain.R
+++ R/annual_rainfall_annual_rain.R
@@ -35,7 +35,6 @@ annual_rainfall_annual_rain <- function(definitions, daily, data_names) {
     definitions$annual_rain$annual_rain <- "FALSE"
   if (definitions$annual_rain$n_rain){
     if (is.null(definitions$annual_rain$rain_day)) {
-      warning("Missing value in annual_rain definitions for rain_day. Setting as 0")
       definitions$annual_rain$rain_day <- 0
     }
   } else {
