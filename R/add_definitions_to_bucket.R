b02d42f1fb2f2df821468b34203200e9868aac6e
lilyclements <lily@idems.international>
2024-04-02 12:55:18 +0100

    updating add_definitions_to_bucket

--- R/add_definitions_to_bucket.R
+++ R/add_definitions_to_bucket.R
@@ -10,6 +10,24 @@
 #' @export
 #' @details
 #' The function creates a timestamp in the format "YYYYMMDDHHMMSS" and appends it to the station_id to form the filename. It then reads the provided JSON file, creates a new JSON file with the timestamped filename, and uploads it to the specified GCS bucket.
-add_definitions_to_bucket <- function(country, station_id, new_definitions, timestamp = NULL){
-  return(epicsadata::add_definitions_to_bucket(country = country, station_id = station_id, new_definitions = new_definitions, timestamp = timestamp))
+add_definitions_to_bucket <- function (country, station_id, new_definitions, timestamp = NULL) 
+{
+  bucket <- get_bucket_name(country)
+  definitions_dir <- "definitions"
+  if (is.null(timestamp)) {
+    timestamp <- format(Sys.time(), format = "%Y%m%d%H%M%S")
+  }
+  new_filename <- paste0(station_id, ".", timestamp, 
+                         ".json")
+  #new_json <- paste0(new_definitions, ".json")
+  # googleCloudStorageR::gcs_upload(file = new_json, bucket = bucket, 
+  #                                 name = file.path(definitions_dir, new_filename))
+  # 
+  object_function <- function(input, output) {
+    jsonlite::write_json(input, path = output, auto_unbox = TRUE, pretty = TRUE)
+  }
+  googleCloudStorageR::gcs_upload(file = new_definitions, bucket = bucket, 
+                                  name = paste0(file.path(definitions_dir, new_filename)),
+                                  object_function = object_function, 
+                                  predefinedAcl = "bucketLevel")
 }
