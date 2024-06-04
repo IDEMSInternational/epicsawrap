84a4ad16080d61e9eb41bfab12870a8fc95f9298
lilyclements <lily@idems.international>
2024-06-04 13:27:33 +0100

    adding ability to get_definitions_id from station_id

--- R/definitions.R
+++ R/definitions.R
@@ -1,7 +1,7 @@
 #' Definitions
 #'
 #' @param country `character(1)` The country code of the data.
-#' @param station_id `character(1)` The station code in the data.
+#' @param definitions_id `character(1)` The definitions code in the data.
 #' @param summaries `character` Vector of summaries to display
 #' @param file Default `NULL` meaning that the most recent definitions file will be found and imported. Otherwise specify as a string the file to import. In format: "STATIONNAME.TIMESTAMP" e.g. "1.20240311152831"
 #'
@@ -10,8 +10,8 @@
 #'
 #' @examples # e.g. definitions("zm", "16", "annual_rain")
 #' # error: definitions("zm", "1", c("annual_rain", "hi", "end_season"))
-definitions <- function(country, station_id, summaries, file = NULL){
-  definition_data <- epicsadata::get_definitions_data(country = country, station_id = station_id, file = file)
+definitions <- function(country, definitions_id, summaries, file = NULL){
+  definition_data <- epicsadata::get_definitions_data(country = country, station_id = definitions_id, file = file)
   definition_data <- purrr::map(.x = summaries, .f = ~ definition_data[[.x]])
   names(definition_data) <- summaries
   # are any NULL 1 = NULL
