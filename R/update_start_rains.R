cb3fa6aedf35b7f2f98160403142d7f7dc2bebd4
lilyclements <lily@idems.international>
2025-10-23 13:56:34 +0100

    Documentation updates - and adding return value

--- R/update_start_rains.R
+++ R/update_start_rains.R
@@ -1,71 +1,12 @@
-#' Safely convert an Object to a Logical value
-#' 
-#' @param x An object to be converted to a logical value If `x` is `NULL`, the function returns `NULL` instead of an error.
-#'
-#' @return
-#' A logical value if conversion is successful, or `NULL` if the input is `NULL`.
-as_logical <- function(x) {
-  if (is.null(x)) {
-    return(FALSE)
-  }
-  as.logical(x)
-}
-
-#' Safely convert an Object to Numeric
-#' 
-#' @param x An object to be converted to numeric. If `x` is `NULL`, the function returns `NULL` instead of an error.
-#'
-#' @return
-#' A numeric value if conversion is successful, or `NULL` if the input is `NULL`.
-as_numeric <- function(x){
-  if (is.null(x)){
-    return(NULL)
-  }
-  as.numeric(x)
-}
-
-
-
 #' Update Start Rains
 #'
 #' @param data_frame A data frame containing the columns specified in `data_names`.
 #' @param data_names A list of column names that are needed from the `data_frame`.
 #' @param definitions A list containing definitions to be read in.
 #' @param data_book The data book object where the data object is stored.
-#'
+#' 
+#' @return A data frame in the data book at the year (and station) level containing start of rains data.
 #' @export
-#'
-#' @examples
-#' library(rpicsa)
-#' library(databook)
-#'
-#' # 1. Let's set up our data book
-#' data_book <- DataBook$new()
-#'
-#' # 2. Importing in some data for testing (this is stored in the rpicsa package)
-#' data(daily_niger)
-#' data_book$import_data(list(daily_niger = daily_niger))
-#' 
-#' # 3. Read in our definitions data
-#' definitions <- jsonlite::fromJSON("C:/Users/HP/Downloads/test_json_1.json")
-#'
-#' # 4. Put in "data_names" the names of all the variables we're going to use from the daily_niger data. 
-#' # Looking at our rpicsa::annual_rain, this can be
-#' # station, year, and rain
-#' data_names <- list(date = "date", 
-#'                    rain = "rain",
-#'                    year = "year", 
-#'                    doy = "doy",
-#'                    station = "station_name")
-#' 
-#' 
-#' \dontrun{
-#'   update_start_rains(data_frame = "daily_niger", 
-#'                      data_names = data_names, 
-#'                      definitions = definitions, 
-#'                      data_book = data_book)
-#' }
-#' 
 update_start_rains <- function(data_frame, data_names, definitions, data_book){
   
     start_rains_definitions <- definitions$annual_summaries$start_rains
