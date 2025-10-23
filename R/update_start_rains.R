6748d005b74f28ec9cd2a15d7d618bc1ed50ed1e
lilyclements <lily@idems.international>
2025-10-23 17:21:37 +0100

    Minor fixes in start/end rains

--- R/update_start_rains.R
+++ R/update_start_rains.R
@@ -9,49 +9,72 @@
 #' @export
 update_start_rains <- function(data_frame, data_names, definitions, data_book){
   
-    start_rains_definitions <- definitions$annual_summaries$start_rains
-    
-    threshold <- as_numeric(start_rains_definitions$threshold)
-    start_day <- as_numeric(start_rains_definitions$start_day)
-    end_day <- as_numeric(start_rains_definitions$end_day)
-    total_rainfall_over_days <- as_numeric(start_rains_definitions$over_days)
-    amount_rain <- as_numeric(start_rains_definitions$amount_rain)
-    prob_rain_day <- as.numeric(start_rains_definitions$prob_rain_day)
-    dry_spell <- as_logical(start_rains_definitions$dry_spell)
-    spell_interval <- as_numeric(start_rains_definitions$spell_interval)
-    spell_max_dry_days <- as_numeric(start_rains_definitions$spell_max_dry_days)
-    dry_period <- as_logical(start_rains_definitions$dry_period)
-    period_interval <- as.numeric(start_rains_definitions$period_interval)
-    max_rain <- as.numeric(start_rains_definitions$max_rain)
-    period_max_dry_days <- as.numeric(start_rains_definitions$period_max_dry_days)
-    
-    start_rains <- rpicsa::start_rains(data=data_frame,
-                                       date_time=data_names$date,
-                                       station=data_names$station,
-                                       year=data_names$year,
-                                       rain=data_names$rain,
-                                       doy=data_names$doy,
-                                       threshold=threshold,
-                                       start_day=start_day,
-                                       end_day=end_day,
-                                       s_start_month = 1, # No value in defintions file. Default used.
-                                       drop = TRUE, # No value in defintions file. Default used.
-                                       output = c("doy", "date", "status"), # No value in defintions file. Default used.
-                                       total_rainfall_over_days = total_rainfall_over_days,
-                                       total_rainfall_comparison = "amount", # No value in defintions file. Default used.
-                                       amount_rain = amount_rain,
-                                       prob_rain_day = prob_rain_day,
-                                       evaporation_variable = NULL, # No value in defintions file. Default used.
-                                       fraction = 0.5, # No value in defintions file. Default used.
-                                       number_rain_days = FALSE, # No value in defintions file. Default used.
-                                       min_rain_days = 1, # No value in defintions file. Default used.
-                                       rain_day_interval = 2, # No value in defintions file. Default used.
-                                       dry_spell = dry_spell,
-                                       spell_interval = spell_interval,
-                                       spell_max_dry_days = spell_max_dry_days,
-                                       dry_period = dry_period,
-                                       period_interval = period_interval,
-                                       max_rain = max_rain,
-                                       period_max_dry_days = period_max_dry_days,
-                                       data_book = data_book)
+  start_rains_definitions <- definitions$annual_summaries$start_rains
+  
+  threshold <- as_numeric(start_rains_definitions$threshold)
+  start_day <- as_numeric(start_rains_definitions$start_day)
+  end_day <- as_numeric(start_rains_definitions$end_day)
+  total_rainfall_over_days <- as_numeric(start_rains_definitions$over_days)
+  amount_rain <- as_numeric(start_rains_definitions$amount_rain)
+  prob_rain_day <- as_numeric(start_rains_definitions$prob_rain_day)
+  
+  proportion <- as_logical(start_rains_definitions$proportion)
+  evaporation <- as_logical(start_rains_definitions$evaporation)
+  if (proportion) total_rainfall_comparison <- "proportion"
+  else if (evaporation) total_rainfall_comparison <- "evaporation"
+  else total_rainfall_comparison <- "amount"
+  # TODO: what about the "evaporation" option? Need to add in evaporation TRUE/FALSE into the definitions file. 
+  evaporation_variable <- start_rains_definitions$evaporation_variable
+  fraction <- as_numeric(start_rains_definitions$fraction)
+  
+  dry_spell <- as_logical(start_rains_definitions$dry_spell)
+  spell_interval <- as_numeric(start_rains_definitions$spell_interval)
+  spell_max_dry_days <- as_numeric(start_rains_definitions$spell_max_dry_days)
+  
+  dry_period <- as_logical(start_rains_definitions$dry_period)
+  period_interval <- as.numeric(start_rains_definitions$period_interval)
+  max_rain <- as.numeric(start_rains_definitions$max_rain)
+  period_max_dry_days <- as.numeric(start_rains_definitions$period_max_dry_days)
+  
+  # TODO: is number of rainy days being read in?
+  number_rain_days = as_logical(start_rains_definitions$number_rain_days)
+  min_rain_days = as_numeric(start_rains_definitions$min_rain_days)
+  rain_day_interval = as_numeric(start_rains_definitions$rain_day_interval)
+  
+  output <- start_rains_definitions$output
+  if (is.null(output) || output == "both") output <- c("doy", "date", "status")
+  s_start_doy <- as.numeric(start_rains_definitions$s_start_doy)
+  s_start_month <- as.numeric(format(as.Date(s_start_doy, origin = "2020-12-31"), "%m"))   # picking a non-leap-year
+  drop <- as_logical(start_rains_definitions$drop)
+  if (length(drop) == 0) drop <- FALSE
+  
+  start_rains <- rpicsa::start_rains(data=data_frame,
+                                     date_time=data_names$date,
+                                     station=data_names$station,
+                                     year=data_names$year,
+                                     rain=data_names$rain,
+                                     doy=data_names$doy,
+                                     threshold=threshold,
+                                     start_day=start_day,
+                                     end_day=end_day,
+                                     s_start_month = s_start_month,
+                                     drop = drop,
+                                     output = output,
+                                     total_rainfall_over_days = total_rainfall_over_days,
+                                     total_rainfall_comparison = "amount", # No value in defintions file. Default used.
+                                     amount_rain = amount_rain,
+                                     prob_rain_day = prob_rain_day,
+                                     evaporation_variable = NULL, # No value in defintions file. Default used.
+                                     fraction = 0.5, # No value in defintions file. Default used.
+                                     number_rain_days = FALSE, # No value in defintions file. Default used.
+                                     min_rain_days = 1, # No value in defintions file. Default used.
+                                     rain_day_interval = 2, # No value in defintions file. Default used.
+                                     dry_spell = dry_spell,
+                                     spell_interval = spell_interval,
+                                     spell_max_dry_days = spell_max_dry_days,
+                                     dry_period = dry_period,
+                                     period_interval = period_interval,
+                                     max_rain = max_rain,
+                                     period_max_dry_days = period_max_dry_days,
+                                     data_book = data_book)
 }
--- inst/update_end_rains_etc.R
+++ inst/update_end_rains_etc.R
@@ -182,3 +182,116 @@ update_monthly_temperature(data_frame = "daily_niger",
                             data_book = data_book)
 
 data_book$get_data_frame("daily_niger_by_station_name_month")
+
+#############################################################
+
+# UPDATE END SEASON =============================================================
+
+library(rpicsa)
+library(databook)
+
+# 1. Let's set up our data book
+data_book <- DataBook$new()
+
+# 2. Importing in some data for testing (this is stored in the rpicsa package)
+data(daily_niger)
+data_book$import_data(list(daily_niger = daily_niger))
+data_book$add_key("daily_niger", c("date", "station_name"), "key")
+
+# 3. Read in our definitions data
+definitions <- jsonlite::fromJSON("C:/Users/lclem/Downloads/test_json_1.json")
+
+definitions$annual_summaries$end_season$start_day <- 1
+definitions$annual_summaries$end_season$end_day <- 200
+definitions$annual_summaries$end_season$capacity <- 50
+definitions$annual_summaries$end_season$water_balance_max <- 100
+definitions$annual_summaries$end_season$evaporation_value <- 5
+definitions$annual_summaries$end_season$reducing_value <- 0.5
+
+# 4. Put in "data_names" the names of all the variables we're going to use from the daily_niger data.
+# Looking at our rpicsa::annual_rain, this can be
+# station, year, and rain
+data_names <- list(date = "date",
+                   rain = "rain",
+                   year = "year",
+                   doy = "doy",
+                   station = "station_name")
+update_end_season(data_frame = "daily_niger",
+                     data_names = data_names,
+                     definitions = definitions,
+                     data_book = data_book)
+data_book$get_data_frame("daily_niger_by_station_name_year")
+
+
+# Now with an evaporation variable ################################
+# 1. Let's set up our data book
+data_book <- DataBook$new()
+
+# 2. Importing in some data for testing (this is stored in the rpicsa package)
+data(daily_niger)
+daily_niger <- daily_niger %>%
+  dplyr::mutate(evap_var = 5,
+                year = as.numeric(year))
+data_book$import_data(list(daily_niger = daily_niger))
+data_book$add_key("daily_niger", c("date", "station_name"), "key")
+
+# 4. Put in "data_names" the names of all the variables we're going to use from the daily_niger data.
+# Looking at our rpicsa::annual_rain, this can be
+# station, year, and rain
+definitions$annual_summaries$end_season$evaporation <- "variable"
+definitions$annual_summaries$end_season$evaporation_value <- NULL
+definitions$annual_summaries$end_season$evaporation_variable <- "evap_var"
+
+data_names <- list(date = "date",
+                   rain = "rain",
+                   year = "year",
+                   doy = "doy",
+                   station = "station_name",
+                   evaporation_variable = "evap_var")
+update_end_season(data_frame = "daily_niger",
+                  data_names = data_names,
+                  definitions = definitions,
+                  data_book = data_book)
+data_book$get_data_frame("daily_niger_by_station_name_year")
+
+
+
+################# START RAINS =========================================================
+
+library(rpicsa)
+library(databook)
+
+# 1. Let's set up our data book
+data_book <- DataBook$new()
+
+# 2. Importing in some data for testing (this is stored in the rpicsa package)
+data(daily_niger)
+data_book$import_data(list(daily_niger = daily_niger))
+
+# 3. Read in our definitions data
+definitions <- jsonlite::fromJSON("C:/Users/lclem/Downloads/test_json_1.json")
+
+# temp add random numbers in here. Fixed it in rpicsa that this is not needed after next sync. 
+definitions$annual_summaries$start_rains$prob_rain_day <- 0.8
+definitions$annual_summaries$start_rains$period_interval <- 3
+definitions$annual_summaries$start_rains$period_max_dry_days <- 1
+definitions$annual_summaries$start_rains$max_rain <- 3
+# 4. Put in "data_names" the names of all the variables we're going to use from the daily_niger data.
+# Looking at our rpicsa::annual_rain, this can be
+# station, year, and rain
+data_names <- list(date = "date",
+                   rain = "rain",
+                   year = "year",
+                   doy = "doy",
+                   station = "station_name")
+
+update_start_rains(data_frame = "daily_niger",
+                     data_names = data_names,
+                     definitions = definitions,
+                     data_book = data_book)
+
+
+
+
+
+
